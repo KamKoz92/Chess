11d31856a44c2b9873c5cc6e23ed9bed9c6b8cd4
ShowiestBug <51924117+ShowiestBug@users.noreply.github.com>
2020-05-15 12:53:13 +0200

    rendering chess pieces

--- Screen.cpp
+++ Screen.cpp
@@ -38,7 +38,10 @@ bool Screen::init() {
         SDL_Quit();
         return false;
     }
-
+    if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        cout << "Failed to initialise SDL_image for PNG" << endl;
+        return false;
+    }
     SetBoard();
     return true;
 }
@@ -106,7 +109,26 @@ void Screen::SetBoard() {
             drawSquare(i,j,100,255,151,53);
         }
     }
-    update();
+    SDL_UpdateTexture(m_texture, NULL, m_buffer1, SCREEN_WIDTH * sizeof(Uint32));
+    SDL_RenderClear(m_renderer);
+    SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
+
+    loadPNG();
+    renderPieces();
+    
+    
+    
+    SDL_RenderPresent(m_renderer);
+
+
+    // m_destination.x += 25;
+    // m_destination.y += 25;
+    // SDL_RenderPresent(m_renderer);
+
+    // SDL_DestroyTexture(test_texture);
+ 
+
+    //update();
 }
 
 void Screen::drawSquare(int x, int y, int size, Uint8 red, Uint8 green, Uint8 blue) {
@@ -120,4 +142,183 @@ void Screen::drawSquare(int x, int y, int size, Uint8 red, Uint8 green, Uint8 bl
             }
         }   
     }
+}
+
+void Screen::loadPNG() {
+    SDL_Surface *imageSurface1 = IMG_Load("pngs/bB.png");
+    bB = SDL_CreateTextureFromSurface(m_renderer, imageSurface1);
+
+    SDL_Surface *imageSurface2 = IMG_Load("pngs/bK.png");
+    bK = SDL_CreateTextureFromSurface(m_renderer, imageSurface2);
+
+    SDL_Surface *imageSurface3 = IMG_Load("pngs/bN.png");
+    bN = SDL_CreateTextureFromSurface(m_renderer, imageSurface3);
+
+    SDL_Surface *imageSurface4 = IMG_Load("pngs/bP.png");
+    bP = SDL_CreateTextureFromSurface(m_renderer, imageSurface4);
+
+    SDL_Surface *imageSurface5 = IMG_Load("pngs/bQ.png");
+    bQ = SDL_CreateTextureFromSurface(m_renderer, imageSurface5);
+
+    SDL_Surface *imageSurface6 = IMG_Load("pngs/bR.png");
+    bR = SDL_CreateTextureFromSurface(m_renderer, imageSurface6);
+
+    SDL_Surface *imageSurface7 = IMG_Load("pngs/wB.png");
+    wB = SDL_CreateTextureFromSurface(m_renderer, imageSurface7);
+
+    SDL_Surface *imageSurface8 = IMG_Load("pngs/wK.png");
+    wK = SDL_CreateTextureFromSurface(m_renderer, imageSurface8);
+
+    SDL_Surface *imageSurface9 = IMG_Load("pngs/wN.png");
+    wN = SDL_CreateTextureFromSurface(m_renderer, imageSurface9);
+
+    SDL_Surface *imageSurface10 = IMG_Load("pngs/wP.png");
+    wP = SDL_CreateTextureFromSurface(m_renderer, imageSurface10);
+
+    SDL_Surface *imageSurface11 = IMG_Load("pngs/wQ.png");
+    wQ = SDL_CreateTextureFromSurface(m_renderer, imageSurface11);
+
+    SDL_Surface *imageSurface12 = IMG_Load("pngs/wR.png");
+    wR = SDL_CreateTextureFromSurface(m_renderer, imageSurface12);
+   
+    if(imageSurface1 == NULL || imageSurface2 == NULL || 
+    imageSurface3 == NULL || imageSurface4 == NULL || 
+    imageSurface5 == NULL || imageSurface6 == NULL || 
+    imageSurface7 == NULL || imageSurface8 == NULL || 
+    imageSurface9 == NULL || imageSurface10 == NULL || 
+    imageSurface11 == NULL || imageSurface12 == NULL) {
+        cout << "Problem with loading pngs" << endl;
+        return;
+    }
+
+    SDL_FreeSurface(imageSurface1);
+    SDL_FreeSurface(imageSurface2);
+    SDL_FreeSurface(imageSurface3);
+    SDL_FreeSurface(imageSurface4);
+    SDL_FreeSurface(imageSurface5);
+    SDL_FreeSurface(imageSurface6);
+    SDL_FreeSurface(imageSurface7);
+    SDL_FreeSurface(imageSurface8);
+    SDL_FreeSurface(imageSurface9);
+    SDL_FreeSurface(imageSurface10);
+    SDL_FreeSurface(imageSurface11);
+    SDL_FreeSurface(imageSurface12);
+}
+
+void Screen::renderPieces() {
+
+    //rendering black pieces
+    SDL_Rect m_destination;
+    m_destination.h = 80;
+    m_destination.w = 80;
+
+    m_destination.x = 30;
+    m_destination.y = 30;
+    SDL_RenderCopy(m_renderer, bR, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bN, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bB, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bQ, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bK, NULL, &m_destination);
+    
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bB, NULL, &m_destination);
+    
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bN, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bR, NULL, &m_destination);
+
+    m_destination.x = 30;
+    m_destination.y = 130;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, bP, NULL, &m_destination);
+
+
+    //rendering white pieces
+    m_destination.x = 30;
+    m_destination.y = 730;
+    SDL_RenderCopy(m_renderer, wR, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wN, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wB, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wQ, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wK, NULL, &m_destination);
+    
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wB, NULL, &m_destination);
+    
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wN, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wR, NULL, &m_destination);
+
+    m_destination.x = 30;
+    m_destination.y = 630;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+    m_destination.x += 100;
+    SDL_RenderCopy(m_renderer, wP, NULL, &m_destination);
+
+
 }
--- Screen.h
+++ Screen.h
@@ -2,6 +2,7 @@
 #define SCREEN_H_
 #include <SDL2/SDL.h>
 #include <memory>
+#include <SDL2/SDL_image.h>
 
 class Screen {
 private:
@@ -10,6 +11,18 @@ private:
     SDL_Texture *m_texture;
     Uint32 *m_buffer1;
     Uint32 *m_buffer2;
+    SDL_Texture *bB;
+    SDL_Texture *bK;
+    SDL_Texture *bN;
+    SDL_Texture *bP;
+    SDL_Texture *bQ;
+    SDL_Texture *bR;
+    SDL_Texture *wB;
+    SDL_Texture *wK;
+    SDL_Texture *wN;
+    SDL_Texture *wP;
+    SDL_Texture *wQ;
+    SDL_Texture *wR;
 
 public:
     const static int SCREEN_HEIGHT = 840;
@@ -24,6 +37,8 @@ public:
     void SetBoard();
     void drawSquare(int x, int y, int size, Uint8 red, Uint8 green, Uint8 blue);
     int Color(Uint8 r, Uint8 g, Uint8 b);
+    void loadPNG();
+    void renderPieces();
 };
 
 #endif /* SCREEN_H */
